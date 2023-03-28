d266e54b0b08af522cc1f650015721fbc5ba0b64
unknown <iambmanohar@gmail.com>
2023-03-28 21:53:47 +0530

    data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
-
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello, Good morning
-      </div>
-    )
-  }
+import Navbar from './Navbar/Navbar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import UserApp from './UserApp/UserApp'
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/user" element={<UserApp />} />
+                    </Routes>
+                </Router>
+            </div>
+        )
+    }
 }
 
 export default App
---
+++ src/UserApp/UserApp/UserApp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class UserApp extends Component {
+  render() {
+    return (
+      <div>UserApp</div>
+    )
+  }
+}
+
+export default UserApp
